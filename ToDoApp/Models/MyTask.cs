2e2fa7be3c14157c67b6575b625c777ee9dd7881
Adam Fanslau <adam.fanslau@gmail.com>
2021-06-10 15:10:58 +0100

    Added entity framework

---
+++ ToDoApp/Models/MyTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoApp.Models
+{
+    public class MyTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Deadline { get; set; }
+
+    }
+}
