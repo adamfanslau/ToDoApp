782c8541c8cc42b0755750dbe0a65b0dbbf35720
Adam Fanslau <adam.fanslau@gmail.com>
2021-06-10 14:53:24 +0100

    Add project files.

---
+++ ToDoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoApp.Startup))]
+namespace ToDoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
